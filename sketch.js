de3e93c744de02be95ac4c9467718f5fb51eb63d
scruffynerf <scruffynerf23@gmail.com>
2021-09-30 21:21:19 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function setup() {
   gravity = createVector(0, 0.35);
   friction = 0.3;
   elasticity = 0.9;
-  magBias = 230000;
+  magBias = 430000;
 
   cnv = createCanvas(width, height);
   var x = (windowWidth - width) / 2;
